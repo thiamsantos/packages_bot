2aab88b1d69ecebbb7512686f67b3db02967adc6
Thiago Santos <thia.mdossantos@gmail.com>
2019-10-11 17:29:38 -0300

    Without timber

--- config/prod.exs
+++ config/prod.exs
@@ -1,13 +1,11 @@
 use Mix.Config
 
-config :packages_bot, PackagesBot.Repo, pool_size: 2
-
 config :logger,
-  backends: [Logger.Backends.Console, Timber.LoggerBackends.HTTP],
+  # backends: [Logger.Backends.Console, Timber.LoggerBackends.HTTP],
   level: :info,
   metadata: [:module],
   format: "$time $metadata[$level] $message\n"
 
-config :timber,
-  api_key: System.get_env("TIMBER_API_KEY"),
-  source_id: System.get_env("TIMBER_SOURCE_ID")
+# config :timber,
+#   api_key: System.get_env("TIMBER_API_KEY"),
+#   source_id: System.get_env("TIMBER_SOURCE_ID")
